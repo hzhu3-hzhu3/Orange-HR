f3a608e9ca21c4877cf3ff7f278f5d095be90df1
hzhu3-hzhu3 <hzhu3@academic.rrc.ca>
2026-01-16 10:21:27 -0600

    Fix: Design-time factory reads DATABASE_URL

--- src/HRManagement.Infrastructure/Data/HRManagementDbContextFactory.cs
+++ src/HRManagement.Infrastructure/Data/HRManagementDbContextFactory.cs
@@ -6,7 +6,22 @@ public class HRManagementDbContextFactory : IDesignTimeDbContextFactory<HRManage
     public HRManagementDbContext CreateDbContext(string[] args)
     {
         var optionsBuilder = new DbContextOptionsBuilder<HRManagementDbContext>();
-        optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=HRManagementDb;Username=postgres;Password=postgres");
+        
+        var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+        string connectionString;
+        
+        if (!string.IsNullOrEmpty(databaseUrl))
+        {
+            var databaseUri = new Uri(databaseUrl);
+            var userInfo = databaseUri.UserInfo.Split(':');
+            connectionString = $"Host={databaseUri.Host};Port={databaseUri.Port};Database={databaseUri.LocalPath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Require;Trust Server Certificate=true";
+        }
+        else
+        {
+            connectionString = "Host=localhost;Port=5432;Database=HRManagementDb;Username=postgres;Password=postgres";
+        }
+        
+        optionsBuilder.UseNpgsql(connectionString);
         return new HRManagementDbContext(optionsBuilder.Options);
     }
 }
